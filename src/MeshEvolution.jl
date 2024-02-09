3a51810fc3fe7fe744a8b67651b1c50280365d29
NiklasEuler <Niklas.Euler@t-online.de>
2024-02-09 16:48:46 +0100

    improved candidates for index manipulation

--- src/MeshEvolution.jl
+++ src/MeshEvolution.jl
@@ -16,22 +16,47 @@ function j2lcmk(N, j)
     j = convert(Int64,j)
 	n_loops = 2
     n_loops2 = 4
-	l = j ÷ (n_loops2 * N)
-	j -= l * n_loops2 * N
-	c = j ÷ (n_loops * N)
-	j -= c * n_loops * N
-	m = j ÷ n_loops
-	j -= m * n_loops
-	k = j
+    l, j = divrem(j, n_loops2 * N)
+	#l = j ÷ (n_loops2 * N)
+	#j -= l * n_loops2 * N
+    c, j = divrem(j, n_loops * N)
+	#c = j ÷ (n_loops * N)
+	#j -= c * n_loops * N
+    m, k = divrem(j, n_loops)
+	#m = j ÷ n_loops
+	#j -= m * n_loops
+	#k = j
 	return l, c, m, k
 end
 
-function shift_timebins(state_vec::Vector)
-    new_vec = Vector{ComplexF64}(undef, length(state_vec)+4)
+function lm2j(N, l, m): # transforms the one index notation of basis elements to the two index notation
+    return l*N+m
+end
+
+function j2lm(N, j): # inverse of lm2j
+    l,m = divrem(j,N)
+    return l,m
+end
+
+#= function shift_timebins(state_vec::Vector)
+    n_loops = 2
+    new_vec = Vector{ComplexF64}(undef, length(state_vec)+n_loops)
     new_vec[2] = 0
     new_vec[end-1] = 0
     new_vec[1:2:end-3] = state_vec[1:2:end]
-    new_vec[2:2:end-1] = state_vec[2:2:end]
+    new_vec[4:2:end] = state_vec[2:2:end]
+    return new_vec
+end =#
+
+function shift_timebins(state_vec::Vector)
+    n_loops = 2
+    N = Int64(sqrt(length(state_vec)/(n_loops)))
+    new_vec = zeros{ComplexF64}(length(state_vec)+n_loops)
+    for j in 1:length(state_vec)
+        l,c,m,k = j2lcmk(N,j)
+        shifted_j = lcmk2j(N, l+c, c, m+k, k)
+        new_vec[shifted_j] = state_vec[j]
+    end
     return new_vec
 end
 
@@ -48,7 +73,14 @@ end
 function coin_operator(angles::Vector)
     real_angles = convert(Vector{Float64}, angles)
     matrices = [beam_splitter_operator(θ) for θ in real_angles]
-    return blockdiag(matrices...)
+    single_photon_coin_operator = blockdiag(matrices...)
+    tensor_coin_operator = kron(single_photon_coin_operator,single_photon_coin_operator)
+    return tensor_coin_operator
 end
 
-#function insert_initial_state()
+function correlated_timebin_state(coeffs::Vec)
+N = length(coeffs)
+
+
+function insert_initial_state()
+end
