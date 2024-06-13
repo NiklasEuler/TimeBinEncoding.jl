ea98afe2a2ca7e98a0bc69de97ffd858c1e3fde3
NiklasEuler <Niklas.Euler@t-online.de>
2024-06-13 16:30:51 +0200

    moved spurious identical-photons test

--- test/index_conversions_tests.jl
+++ test/index_conversions_tests.jl
@@ -68,36 +68,3 @@ end
     @test_throws ArgumentError lc2j(1, -1)
     @test_throws ArgumentError lc2j(4, 2)
 end
-
-@testset "lcmk <-> j identical" begin
-
-    N = 7
-    d_hilbert_space = Int(N_LOOPS * N * (N_LOOPS * N + 1) / 2)
-
-    j_reconstr = zeros(Int64,d_hilbert_space)
-	for i in 1:d_hilbert_space
-		idxs = j2lcmk_identical(N, i)
-		j_reconstr[i] = lcmk2j_identical(N, idxs...)
-	end
-
-    @test 1:(d_hilbert_space) == j_reconstr
-    @test j2lcmk_identical(N, j_reconstr[end]) == (N - 1, 1, N - 1, 1)
-    @test j2lcmk_identical(N, 1) == (0, 0, 0, 0)
-    @test j2lcmk_identical(N, 61) == (2, 1, 2, 1)
-
-    N = 12
-    d_hilbert_space = Int(N_LOOPS * N * (N_LOOPS * N + 1) / 2)
-
-    j_reconstr = zeros(Int64, d_hilbert_space)
-	for i in 1:d_hilbert_space
-		idxs = j2lcmk_identical(N, i)
-		j_reconstr[i] = lcmk2j_identical(N, idxs...)
-	end
-
-    @test 1:(d_hilbert_space) == j_reconstr
-    @test j2lcmk_identical(N, j_reconstr[end]) == (N - 1, 1, N - 1, 1)
-    @test j2lcmk_identical(N, 1) == (0, 0, 0, 0)
-    @test j2lcmk_identical(N, 70) == (1, 1, 1, 1)
-
-    @test_throws ArgumentError lcmk2j_identical(N, 5, 1, 5, 0)
-end
