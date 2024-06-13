4324a0ba4e26ea2f64533ae6fd5f9b4373e98b27
NiklasEuler <Niklas.Euler@t-online.de>
2024-06-13 17:01:04 +0200

    file tree restructure to isolate multi-spdc functions into own src files

--- src/TimeBinEncoding.jl
+++ src/TimeBinEncoding.jl
@@ -18,4 +18,18 @@ include("StateUtility.jl") # Setup and tools for quantum wave functions and dens
 include("SymbolicEvolution.jl") # Symbolic computing tools for time evolution
 include("Visualization.jl") # Visualization for explicit and symbolic results
 
+include("identical_photons/IndexConversionsIdentical.jl")
+# Basis index conversion tools for identical photons
+include("identical_photons/VisualizationIdentical.jl")
+# Visualization for identical photons
+include("identical_photons/CoherenceExtractionIdentical.jl")
+# Extraction of coherence information from measured data for identical photons
+include("identical_photons/ExplicitEvolutionIdentical.jl")
+# Explicit evolution based on analytical computations for identical photons
+include("identical_photons/MeshEvolutionIdentical.jl")
+# Numerical evolution of quantum states in the mesh lattice for identical photons
+include("identical_photons/StateUtilityIdentical.jl")
+# Setup and tools for quantum wave functions and density matrices for identical photons
+
+
 end
