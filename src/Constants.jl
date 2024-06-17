bd866b3eb41552845ccd17a43d43d2ff29811764
NiklasEuler <Niklas.Euler@t-online.de>
2024-06-17 18:42:50 +0200

    added specific beam-splitter angles as constants for future use

--- src/Constants.jl
+++ src/Constants.jl
@@ -1,4 +1,4 @@
-export N_LOOPS, N_LOOPS2, WEIGHT_CUTOFF
+export N_LOOPS, N_LOOPS2, WEIGHT_CUTOFF, Θ, Φ
 
 """
 Number of fiber loops. Saved as const to avoid magic numbers.
@@ -14,3 +14,15 @@ const global N_LOOPS2 = 4
 Numerical cutoff value to determine wether a certain coefficient is ≈ 0
 """
 global const WEIGHT_CUTOFF = 1e-16
+
+"""
+Angle such that sin(Θπ) = 1/√3, used in simplified beam-splitter settings. Saved as const to
+avoid magic numbers.
+"""
+global const Θ = asin(sqrt(1/3))/π
+
+"""
+Angle such that sin(Φπ) = 2/√3, used in simplified beam-splitter settings. Saved as const to
+avoid magic numbers.
+"""
+global const Φ = asin(sqrt(2/3))/π
