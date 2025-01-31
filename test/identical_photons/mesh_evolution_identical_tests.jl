09386f328c7ab79e09e4252fb9fc971a9bbc909d
NiklasEuler <Niklas.Euler@t-online.de>
2025-01-31 16:44:54 +0100

    further single species mesh evolution tests

--- test/identical_photons/mesh_evolution_identical_tests.jl
+++ test/identical_photons/mesh_evolution_identical_tests.jl
@@ -1,10 +1,9 @@
 @testset "mesh_evolution_identical" begin
     N = 4
 
-    d_local_hs_b = Int(N * (N + 1) / 2) # hilbert-space dimension of two identical photons in N time bins. No notion of short/long
     d_local_hs_bl = N * (2 * N + 1)
-    d_full_hs_b = d_local_hs_b ^ 2
-    d_full_hs_bl = d_local_hs_bl ^ 2 # hilbert-space dimension of 2 species a 2 photons in full short/long notion
+    d_full_hs_bl = d_local_hs_bl ^ 2
+        # hilbert-space dimension of 2 species a 2 photons in full short/long notion
 
     Ψ_init = spzeros(ComplexF64, d_full_hs_bl)
 	j = lcmk2j_super_identical(N, 0, 0, 1, 0, 0, 0, 1, 0)
@@ -31,10 +30,64 @@
     Ψ_in_dense = Vector(Ψ_init)
     Ψ_out_dense = mesh_evolution_identical(Ψ_in_dense, angles)
     @test Ψ_out ≈ Ψ_out_dense
-    ρ_out_manual = density_matrix(Ψ_out)
 
+    ρ_out_manual = density_matrix(Ψ_out)
     ρ_in_manual = density_matrix(Ψ_init)
     ρ_out = mesh_evolution_identical(ρ_in_manual, angles)
     @test ρ_out ≈ ρ_out_manual
+
+    ρ_out_mem = mesh_evolution_identical(ρ_in_manual, angles, kron_mem)
+    @test ρ_out_mem ≈ ρ_out
+
+    N = 5
+    d_local_hs_bl = N * (2 * N + 1)
+
+    Ψ_init_sp = spzeros(ComplexF64, d_local_hs_bl)
+    j = lcmk2j_identical(N, 1, 0, 3, 0)
+	Ψ_init_sp[j] = 1
+
+    angles = [zeros(n) for n in N:N+2]
+	angles[1][2] = 0.5π
+	angles[3][4] = 0.25π
+
+    Ψ_out_sp =  mesh_evolution_sp_identical(Ψ_init_sp, angles)
+    @test sum(abs2, Ψ_out_sp) ≈ 1 # normalization
+
+    j1 = lcmk2j_identical(N + 3, 3, 0, 3, 0)
+    j2 = lcmk2j_identical(N + 3, 4, 1, 4, 1)
+
+    @test Ψ_out_sp[[j1, j2]] ≈ [-1/sqrt(2), -1/sqrt(2)] # Hong-Ou-Mandel effect
+
+    Ψ_in_dense_sp = Vector(Ψ_init_sp)
+    Ψ_out_dense_sp = mesh_evolution_sp_identical(Ψ_in_dense_sp, angles)
+    @test Ψ_out_sp ≈ Ψ_out_dense_sp
+
+    ρ_out_manual_sp = density_matrix(Ψ_out_sp)
+    ρ_in_manual_sp = density_matrix(Ψ_init_sp)
+    ρ_out_sp = mesh_evolution_sp_identical(ρ_in_manual_sp, angles)
+    @test ρ_out_sp ≈ ρ_out_manual_sp
+
+    d_full_hs_bl = d_local_hs_bl ^ 2
+    # hilbert-space dimension of 2 species a 2 photons in full short/long notion
+
+    Ψ_init = spzeros(ComplexF64, d_full_hs_bl)
+    j = lcmk2j_super_identical(N, 1, 0, 3, 0, 1, 0, 3, 0)
+	Ψ_init[j] = 1
+
+    Ψ_out =  mesh_evolution_identical(Ψ_init, angles)
+    Ψ_out_sp_kron = kron(Ψ_out_sp, Ψ_out_sp)
+    @test Ψ_out ≈ Ψ_out_sp_kron
+
+    angles = [zeros(n) for n in N:N+3]
+
+    Ψ_out_sp =  mesh_evolution_sp_identical(Ψ_init_sp, angles)
+
+    N = 9
+    d_local_hs_bl = N * (2 * N + 1)
+    Ψ_out_sp_manual = spzeros(ComplexF64, d_local_hs_bl)
+    j = lcmk2j_identical(N, 1, 0, 3, 0)
+    Ψ_out_sp_manual[j] = 1
+    @test Ψ_out_sp ≈ Ψ_out_sp_manual
+
 
 end
